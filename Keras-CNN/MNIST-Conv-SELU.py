bc0b1d7830be732fb705ddf54536e486c85a135b
Guenter Klambauer <klambauer@bioinf.jku.at>
2017-10-31 09:47:34 +0100

    minor comm

--- Keras-CNN/MNIST-Conv-SELU.py
+++ Keras-CNN/MNIST-Conv-SELU.py
@@ -1,12 +1,5 @@
 # Adapted KERAS tutorial 
 
-'''Trains a simple convnet on the MNIST dataset.
-Gets to 99.25% test accuracy after 12 epochs
-(there is still a lot of margin for parameter tuning).
-16 seconds per epoch on a GRID K520 GPU.
-'''
-
-
 from __future__ import print_function
 import keras
 from keras.datasets import mnist
